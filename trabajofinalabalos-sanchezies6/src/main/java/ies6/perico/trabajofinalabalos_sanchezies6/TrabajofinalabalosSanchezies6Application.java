bd6a6a44791a6e1ec4e066104f4ff1cc17fc7466
franco40637 <franco.13.rodrigo@gmail.com>
2025-11-01 20:57:22 -0300

    primer commit

---
+++ trabajofinalabalos-sanchezies6/src/main/java/ies6/perico/trabajofinalabalos_sanchezies6/TrabajofinalabalosSanchezies6Application.java
@@ -0,0 +1,13 @@
+package ies6.perico.trabajofinalabalos_sanchezies6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajofinalabalosSanchezies6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajofinalabalosSanchezies6Application.class, args);
+	}
+
+}
---
+++ trabajofinalabalos-sanchezies6/src/test/java/ies6/perico/trabajofinalabalos_sanchezies6/TrabajofinalabalosSanchezies6ApplicationTests.java
@@ -0,0 +1,13 @@
+package ies6.perico.trabajofinalabalos_sanchezies6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajofinalabalosSanchezies6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
